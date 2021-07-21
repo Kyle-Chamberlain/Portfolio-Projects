d2791a923255778a5456552ac8fc007356ac1974
Noah Jackson <noahjackson446@gmail.com>
2021-07-21 15:47:32 +0800

    Added stylesheet

--- Drawing/index.php
+++ Drawing/index.php
@@ -4,10 +4,11 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>index</title>
-        <h1>Welcome To Your Future Home</h1>
+        <title>Home</title>
+        <link rel="stylesheet" href="Stylesheets/index.css">
     </head>
     <body>
+        <h1>Welcome To Your Future Home</h1>
         <script type="text/javascript" src="grid.js"></script>
         <div id="NavigationBar" >
             <a href="index.html">Home</a>
