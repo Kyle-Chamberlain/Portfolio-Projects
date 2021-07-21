38727a089a06603dfe60605c65c4434531bde659
Kyle-Chamberlain <75663591+Kyle-Chamberlain@users.noreply.github.com>
2021-07-21 15:05:22 +0800

    file structure fixing

--- Drawing/index.php
+++ Drawing/index.php
@@ -4,12 +4,14 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>index</title>
+        <h1>Welcome To Your Future Home</h1>
     </head>
     <body>
         <script type="text/javascript" src="grid.js"></script>
-        <div id="NavBar" class="NavigationBar">
-            <h1>Welcome To Your Future Home</h1>
+        <div id="NavigationBar" >
+            <a href="index.html">Home</a>
+           <a href="logIn.php">Log-In</a>
         </div>
     </body>
 </html>
