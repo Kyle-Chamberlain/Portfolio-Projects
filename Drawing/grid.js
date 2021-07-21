dbe529a9df8d9b691bf9e7ae347d5ba436a20aea
Kyle-Chamberlain <75663591+Kyle-Chamberlain@users.noreply.github.com>
2021-07-21 14:40:50 +0800

    First Upload
    Includes an index page and javascript with css

---
+++ Drawing/grid.js
@@ -0,0 +1,14 @@
+/***************************************************/
+//Test grid array
+var testx = 10;
+var testy = 10;
+
+var gridx = new Array(testx);
+
+for(var i = 0; i < gridx.length; i++) {
+    gridx[i] = new Array(testy);
+}
+
+console.log(gridx);
+/***************************************************/
+
